8e3af09f4ceb606ddedb09881f9e66b2ae4ab838
Chun Yang <acyang97@gmail.com>
2020-08-29 23:03:52 +0800

    Create branch

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,7 +16,7 @@ public class Duke {
         Task taskToBeMarkedAsDone = listOfTasks[indexOfTask - 1];
         taskToBeMarkedAsDone.setDone();
         listOfTasks[indexOfTask - 1] = taskToBeMarkedAsDone;
-        System.out.println("Nice! I've marked this task as done: ");
+        System.out.println("Nice! I've marked this task as done:");
         System.out.println(taskToBeMarkedAsDone);
     }
 

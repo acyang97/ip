5ca919d03358e8a67c3a28033384f26525535f90
Chun Yang <acyang97@gmail.com>
2020-09-02 02:07:09 +0800

    Created AddTaskCommand class

---
+++ src/main/java/AddTaskCommand.java
@@ -0,0 +1,12 @@
+public class AddTaskCommand extends Command {
+    private String commandMethod;
+
+    public AddTaskCommand(String lineInput, String commandMethod) {
+        super(lineInput);
+        this.commandMethod = commandMethod;
+    }
+
+    public void performCommand(TaskManager taskManager) {
+        taskManager.addTask(this.commandMethod, super.getCommand());
+    }
+}

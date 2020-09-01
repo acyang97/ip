71bf2773c9f644588440b62b3c42e2f2a9e97f78
Chun Yang <acyang97@gmail.com>
2020-09-02 02:06:57 +0800

    Created Command class

---
+++ src/main/java/Command.java
@@ -0,0 +1,13 @@
+public abstract class Command {
+    private String command;
+
+    public Command(String command) {
+        this.command = command;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public abstract void performCommand(TaskManager taskManager);
+}

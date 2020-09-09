fd27f56b35604b1488612d878117de122b1b0ddf
Chun Yang <acyang97@gmail.com>
2020-09-09 14:56:42 +0800

    Fix Exception handling so that program wont terminate if catch exception

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -6,26 +6,24 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        try {
-            Scanner sc = new Scanner(System.in);
-            TaskManager taskManager = new TaskManager();
-            System.out.println("Hello! I'm duke.Duke");
-            System.out.println("What can I do for you?");
-            String nextLine = sc.nextLine();
-            LineParser lineParser = new LineParser(nextLine);
-            Command command = lineParser.getTypeOfCommand();
 
-            while (true) {
+        Scanner sc = new Scanner(System.in);
+        TaskManager taskManager = new TaskManager();
+        System.out.println("Hello! I'm duke.Duke");
+        System.out.println("What can I do for you?");
+
+        while (true) {
+            try {
+                String nextLine = sc.nextLine();
+                LineParser lineParser = new LineParser(nextLine);
+                Command command = lineParser.getTypeOfCommand();
                 command.performCommand(taskManager);
                 if (taskManager.getTaskManagerSaidBye()) {
                     break;
                 }
-                nextLine = sc.nextLine();
-                lineParser = new LineParser(nextLine);
-                command = lineParser.getTypeOfCommand();
+            } catch (DukeException e) {
+                System.out.println(e);
             }
-        } catch (DukeException e) {
-            System.out.println(e);
         }
     }
 }

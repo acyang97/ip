838a53a5d9bfd7aa7f6cbda48a573714625f8f8c
Chun Yang <acyang97@gmail.com>
2020-09-21 01:47:21 +0800

    Added JavaDocs

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -1,5 +1,8 @@
 package duke;
 
+/**
+ * DukeException class to handle exceptions in the Duke app.
+ */
 public class DukeException extends Exception {
     public DukeException(String error) {
         super(error);
